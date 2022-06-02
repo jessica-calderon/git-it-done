c3589c62e02428569fdb3b82507e58cca9b2f89d
jessica-calderon <calderonjessica13@yahoo.com>
2022-06-02 02:09:32 -0500

    see more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,15 @@
+var limitWarningEl = document.querySelector("#limit-warning");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 var issueContainerEl = document.querySelector("#issues-container");
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -41,8 +53,13 @@ fetch(apiUrl)
         response.json().then(function(data) {
             // pass response data to dom function
             displayIssues(data);
-        });
-    }
+
+            // check if api has paginated issues
+            if (response.headers.get("link")) {
+                displayWarning(repo);
+            }
+            });
+        }
     else {
         alert("There was a problem with your request!");
     }
